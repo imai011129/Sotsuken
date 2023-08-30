81f38a5593848345f59cb2673e2182881d2afe72
imai011129 <wxpy88@gmail.com>
2023-08-30 23:26:34 +0900

    add file

--- Guide-mode/main.py
+++ Guide-mode/main.py
@@ -1,3 +1,16 @@
+import sympy as sym
+import math
+
 # 定数定義
 a = 1.0
+w = 0.8 * a
 c = 3.0 * 10**8
+epsilon = 12.0
+k_x = 1.0
+
+x = sym.Symbol('x')
+f = x * sym.tan(x * w / 2) 
+g = sym.sqrt((1 - 1 / epsilon) * k_x - x**2 / epsilon)
+solutions = sym.solve(f - g, x)
+
+print(solutions)
